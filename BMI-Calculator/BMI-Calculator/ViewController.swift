b507186ca1af2cb77048349ba3a385c67022f058
stefidka <stef.vacheva@gmail.com>
2021-01-13 12:33:50 +0200

    init bmi calculator project & add assets

---
+++ BMI-Calculator/BMI-Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI-Calculator
+//
+//  Created by Stefka Vacheva on 13.01.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
